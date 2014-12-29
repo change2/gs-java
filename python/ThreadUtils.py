51c70bad00a1bf033f791345162eb4ab9dafa7fe
changejava <changejava@126.com>
2014-12-29 20:49:30 +0800

    add python and ruby support

---
+++ python/ThreadUtils.py
@@ -0,0 +1,17 @@
+__author__ = 'changejava'
+import time
+import random
+import os
+
+time.sleep(5 * random.random())
+
+l = os.listdir('c:/')
+for item in l:
+    print(item)
+l = filter(lambda x: x.endswith('.html'), os.listdir('c:/'))
+for item in l:
+    print(item)
+
+l = filter(lambda y: y.endswith('.html'), os.listdir('c:/'))
+for item in l:
+    print(item)
