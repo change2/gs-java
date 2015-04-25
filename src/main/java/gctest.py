1797cc3d68b0e43be37d6dc2d7a552e199d109ed
changejava <changejava@gmail.com>
2015-04-25 21:55:50 +0800

    python gc collect test

--- src/main/java/gctest.py
+++ src/main/java/gctest.py
@@ -1,1 +1,21 @@
 __author__ = 'changejava'
+import gc
+
+
+class Fruit:
+    def __init__(self, name, color):
+        self.__name = name
+        self.__color = color
+
+    def getColor(self):
+        return self.__color;
+
+    def setColor(self, color):
+        self.__color = color
+
+
+if __name__ == '__main__':
+    fruit = Fruit('zhangsan','red')
+    print gc.get_referents(fruit)
+    del fruit
+    print gc.collect()
